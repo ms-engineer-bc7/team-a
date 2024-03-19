4a2a4d7abc542601bec3a7320ad38641b9782f41
NorikoIshido <norisip@gmail.com>
2024-03-19 14:24:53 +0900

    Flask Hello World!表示できた

--- back/app.py
+++ back/app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 #host='0.0.0.0', port=5000
