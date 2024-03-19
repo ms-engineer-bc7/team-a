127d65a4872f0b6cb75a3d966b026ddadf11e2f1
NorikoIshido <norisip@gmail.com>
2024-03-19 20:29:02 +0900

    POSTエンドポイント追加

--- back/app.py
+++ back/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import random
+from flask import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://quotes:quotes@db:5432/quotesdb'
@@ -21,8 +22,19 @@ if __name__ == '__main__':
 
 
 
-
-
+@app.route('/quotes/<int:id>', methods=['PUT'])
+def update_quote(id):
+    quote = Quote.query.get(id)
+    if quote:
+        new_text = request.json.get('text')
+        if new_text:
+            quote.text = new_text
+            db.session.commit()
+            return jsonify({'message': 'Quote updated successfully'}), 200
+        else:
+            return jsonify({'error': 'Text field is required'}), 400
+    else:
+        return jsonify({'error': 'Quote not found'}), 404
 
 
 
