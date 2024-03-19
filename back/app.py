0fc79b50d8a5b2e0dcdfa55d15b8a1cb2469aa8d
MoeNakamura <154479183+MoeNakamura@users.noreply.github.com>
2024-03-19 20:16:34 +0900

    Revert "ORM記述"

--- back/app.py
+++ back/app.py
@@ -1,15 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from model import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://quotes:quotes@db/quotesdb'
-
-# db オブジェクトの初期化
-db.init_app(app)
-migrate = Migrate(app, db)
-
 
 @app.route('/')
 def index():
@@ -17,5 +8,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
+#host='0.0.0.0', port=5000
