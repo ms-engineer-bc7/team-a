b290188314cb99a6778d0853fbab35d6d2c464b6
Moe Nakamura <moe.aow124@gmail.com>
2024-03-19 19:57:46 +0900

    ORM記述
    ORM記述　Dockerfile変更

--- back/app.py
+++ back/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from model import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://quotes:quotes@db/quotesdb'
+
+# db オブジェクトの初期化
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
@@ -8,4 +17,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-#host='0.0.0.0', port=5000
+
+
